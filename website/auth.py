0542dec34f81251db74eb25e7872fdbd7d8f3675
Anna Xie <79870263+PhoqueTenTen@users.noreply.github.com>
2024-03-23 17:09:47 -0500

    Update auth.py

--- website/auth.py
+++ website/auth.py
@@ -4,7 +4,7 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
-    return render_template("login.html")
+    return render_template("login.html", text="Testing")
 
 @auth.route('/logout')
 def logout():
