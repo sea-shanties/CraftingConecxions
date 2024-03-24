d80aa915663fd2a544d08bd8f0904fc0cfa06ea8
raspberryhelp <152838361+raspberryhelp@users.noreply.github.com>
2024-03-24 07:16:29 -0500

    Update __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -1,13 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
+
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'secret'
+    app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
@@ -22,5 +24,18 @@ def create_app():
     with app.app_context():
         db.create_all()
 
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
     return app
 
+
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created Database!')
